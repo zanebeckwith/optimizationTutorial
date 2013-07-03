ad4001051a987724bfa10f26e104f1763d24993a
Zane Beckwith <zbeckwit@physics.unc.edu>
2013-07-02 21:28:46 -0400

    Working versions of src/scatter.py and src/neldermead.py.
      Though there are docstrings, the comments for neldermead.py
    were stripped after I had to un-compile it after accidentally
    deleting it.

--- src/neldermead.py
+++ src/neldermead.py
@@ -1,168 +1,111 @@
-#!/usr/bin/env python
-
 """
-This file implements a function to determine
-the minimum of the input function 'func' using 
-the Nelder-Mead (downhill simplex) method.
+This file implements a function to find the location of the minimum
+for a given well-behaved non-linear function in arbitrary dimensions
+using the Nelder-Mead (downhill simplex) method.
 
-Originally written for the UNC-CH CAP-REU
-program, summer of 2013.
+Originally written for the UNC-CH CAP-REU program, 
+summer of 2013.
 
 Author: Zane Beckwith
 Adapted from code provided by Dr. Fabian Heitsch.
 
-input :
-	initial_simplex	: list, with n+1 members, each member
-		is a list of n values, where n is the dimension of func
-	func						: the objective function. Must accept n-vector
-	max_iterations	: maximum number of iterations used
-	tol  						: tolerance (applied on f)
-	iverb						: 0 = no diagnostics
-			 						  1 = print out progress
-			 						  2 = show map with each simplex
-output :
-	xmin 						: location of the minimum 
-										(center of gravity of final simplex).
 """
 
-def minimize(initial_simplex, max_iterations, tol, iverb):
-  n    = size(initial_simplex)
-  fcur = zeros(1,n(2))   % n+1 function values (current)
-  fsor = zeros(1,n(2))   % sorted function values
-  scur = initial_simplex         % initialize with first simplex
-  ssor = zeros(n(1),n(2)) % sorted simplex positions.
-  xmin = zeros(n(1),1)
-  xref = zeros(n(1),1) % reflected point
-  xexp = zeros(n(1),1) % expanded point
-  xcon = zeros(n(1),1) % contracted point
-  x0   = zeros(n(1),1)
-  x1   = zeros(n(1),1)
-  fref = 0.0
-  fexp = 0.0
-  fcon = 0.0
-  dx   = 0.0  % keeps track of largest distance between simplex points
-  dxs  = 0.0
-  istol= 0
-  done = 0    % keeps track of whether we're done within one iteration
-  it   = 0
-  jmax = 0
+import numpy as np
+import sys
 
-% these are the coefficients for 
-  alpha= 1.0 % reflection
-  gamma= 2.0 % expansion
-  rho  = 0.5 % contraction
-  sigma= 0.5 % reduction
+def max_distance(points):
+  """
+  Takes a list of 
+  Numpy arrays and finds
+  the largest (Euclidean) distance between them
 
-% draw the starting simplex
-  if (iverb == 2)
-    color('white')
-    plot([initial_simplex(1,1) initial_simplex(1,2)],[initial_simplex(2,1) initial_simplex(2,2)],'w')
-    plot([initial_simplex(1,2) initial_simplex(1,3)],[initial_simplex(2,2) initial_simplex(2,3)],'w')
-    plot([initial_simplex(1,3) initial_simplex(1,1)],[initial_simplex(2,3) initial_simplex(2,1)],'w')
-  end
-  pause(1.0)
+  function: max_distance
+  input :
+  """
+  from math import sqrt
+  distances = []
+  for (index1, p1,) in enumerate(points):
+    for p2 in points[index1:]:
+      distances.append(sqrt(sum((p1 - p2) ** 2)))
 
-  while ((it < max_iterations) && (istol == 0))
-    done = 0 % start again
-%   step 1: calculate f(x) for all vertices, and sort
-    for j=1:n(2)
-      fcur(j) = fxy(scur(:,j))
-    end
-    [fsor indx] = sort(fcur)  % fsor = fcur(indx) 
-    ssor        = scur(:,indx)% re-index the positions 
-%   step2: calculate the center of gravity x0 for all points except the worst x(n+1)
-    sumf = sum(fsor(1:n(2)-1))
-    for i=1:n(1)
-      x0(i) = sum(fsor(1:n(2)-1).*ssor(i,1:n(2)-1))/sumf
-    end 
-    if (iverb > 0)
-      fprintf(1,'minimize: it=%4d: centroid   : f = %13.5e\n',it+1,fxy(x0))
-    end
-%   step3: reflection
-    xref = x0+alpha.*(x0-ssor(:,n(2)))
-    fref = fxy(xref)
-    if ((fsor(1) <= fref) && (fref < fsor(n(1))))
-      if (iverb > 0)
-        fprintf(1,'minimize: it=%4d: reflection : f = %13.5e\n',it+1,fref)
-      end
-      ssor(:,n(2)) = xref(:)
-      done         = 1
-    end
-%   step4: expansion
-    if (done == 0)
-      if (fref < fsor(1))
-        xexp = x0+gamma.*(x0-ssor(:,n(2)))
-        fexp = fxy(xexp)
-        if (iverb > 0)                                                                                                              
-          fprintf(1,'minimize: it=%4d: expansion  : f = %13.5e\n',it+1,fexp)
-        end                                                                                                                         
-        if (fexp < fref)
-          ssor(:,n(2)) = xexp(:)
-          fsor(n(2))   = fexp
-        else
-          ssor(:,n(2)) = xref(:)
-          fsor(n(2))   = fref
-        end
-        done      = 1
-      end
-    end
-%   step5: contraction
-    if (done == 0) 
-      if (fref < fsor(n(2)-1))
-        fprintf(1,'minimize: invalid branch in step 5\n')
-        stop
-      end
-      xcon = ssor(:,n(2))+rho.*(x0-ssor(:,n(2)))
-      fcon = fxy(xcon)
-      if (fcon < fsor(n(2)))
-        if (iverb > 0)                                                                                                                
-          fprintf(1,'minimize: it=%4d: contraction: f = %13.5e\n',it+1,fcon)                                                                        
-        end
-        ssor(:,n(2)) = xcon(:)
-        fsor(n(2))   = fcon
-        done         = 1
-      end
-    end
-%   step6: reduction
-    if (done == 0) 
-      if (iverb > 0)
-        fprintf(1,'minimize: it=%4d: reduction\n',it+1)
-      end
-      for j=2:n(2)
-        ssor(:,j) = ssor(:,1)+sigma.*(ssor(:,j)-ssor(:,1))
-        fsor(j)   = fxy(ssor(:,j))
-      end
-    end
-%  still need to compare current f etc (all steps).
-    scur(:,:) = ssor(:,:) % need to write back the modified simplex as new input
-    it = it+1
-%  calculate largest distance between points: this is not recommended for real applications...
-    dx = -1e10
-    for i=1:n(1)
-      for   j=i+1:n(2)
-        dxs = sqrt(sum((ssor(:,i)-ssor(:,j)).^2))
-        if (dxs > dx)
-          dx   = dxs
-          jmax = j
-        end
-      end
-    end
-    if (dx < tol) 
-      istol = 1
-      xmin  = ssor(:,jmax)
-    end
-    fprintf(1,'minimize: it=%4d: |dx| = %13.5e\n',it,dx)
-    
-%   for visualization: plot most recent simplex
-    if (iverb == 2)
-      color('white')
-      plot([ssor(1,1) ssor(1,2)],[ssor(2,1) ssor(2,2)],'w') 
-      plot([ssor(1,2) ssor(1,3)],[ssor(2,2) ssor(2,3)],'w')
-      plot([ssor(1,3) ssor(1,1)],[ssor(2,3) ssor(2,1)],'w')
-    end
-    pause(1.0)
-  end
-  if (it == max_iterations)
-    fprintf(1,'minimize: reached maximum number of iterations')
-  end
-end
+  return max(distances)
+
+def minimize(func, initial_simplex, max_iterations, tolerance):
+    """
+    Finds the location of the minimum of given function
+    using the Nelder-Mead method.
+  
+    Arguments:
+    initial_simplex   -- list with n+1 members; each member
+                        is a Numpy array of n values, 
+                        where n is the dimension of func()
+    func              -- the objective function. Must accept length-n Numpy array
+    max_iterations    -- maximum number of iterations used
+    tolerance         -- maximum distance between points in final simplex
+    Return value :
+    xmin              -- Location of the minimum as Numpy array
+    """
+
+    alpha = 1.0
+    gamma = 2.0
+    rho = 0.5
+    sigma = 0.5
+
+    values_and_points = [ [func(p), p] for p in initial_simplex ]
+
+    iterations = 0
+
+    while max_distance([ p[1] for p in values_and_points ]) > tolerance:
+      if iterations == max_iterations:
+        print 'minimize: reached maximum number of iterations'
+        break
+      iterations += 1
+      values_and_points.sort()
+      lowest_values_and_points = values_and_points[:-1]
+      highest_value_and_point = values_and_points[-1]
+      center_of_gravity = sum( \
+                             [np.multiply(p[0], p[1]) for p in lowest_values_and_points] \
+                             ) / sum([p[0] for p in lowest_values_and_points])
+      reflected_point = center_of_gravity + \
+          alpha * (center_of_gravity - highest_value_and_point[1])
+      reflected_value = func(reflected_point)
+      if values_and_points[0][0] <= reflected_value <= values_and_points[-1][0]:
+        values_and_points[-1] = [reflected_value, reflected_point]
+        continue
+      if reflected_value < values_and_points[0][0]:
+        expanded_point = center_of_gravity + \
+            gamma * (center_of_gravity - highest_value_and_point[1])
+        expanded_value = func(expanded_point)
+      if expanded_value < reflected_value:
+        values_and_points[-1] = [expanded_value, expanded_point]
+      else:
+        values_and_points[-1] = [reflected_value, reflected_point]
+      continue
+      contracted_point = values_and_points[-1][1] + \
+          rho * (center_of_gravity - highest_value_and_point[1])
+      contracted_value = func(contracted_point)
+      if contracted_value < values_and_points[-1][0]:
+        values_and_points[-1] = [contracted_value, contracted_point]
+        continue
+      for p in values_and_points:
+        p[1] = values_and_points[0][1] + \
+            np.multiply(sigma, p[1] - values_and_points[0][1])
+        p[0] = func(p[1])
+
+    return sorted(values_and_points)[0][1]
+
+
+if __name__ == '__main__':
+
+    def parabola(params):
+        return (params[0] - 0.5) ** 2 + (params[1] + 0.3) ** 2
+
+    known_min = np.array([0.5, -0.3])
+    assert parabola(known_min) == 0.0
+    i_simp = [np.array([-2.0, -2.0]), np.array([0.0, 2.0]), np.array([2.0, -2.0])]
+    max_iters = 500
+    tol = 1e-05
+    min_pos = minimize(parabola, i_simp, max_iters, tol)
+    assert max_distance([min_pos, known_min]) < tol
+
--- src/scatter.py
+++ src/scatter.py
@@ -1,35 +1,50 @@
-#!/usr/bin/env python
-
 """
-This file implements a python function 
-that is intended to be used as part of
+This file implements a python function that is intended to be used as part of
 a tutorial on mathematical optimization.
+
+Originally written for the UNC-CH CAP-REU program, 
+summer of 2013.
 
 Author: Zane Beckwith
 
-input :
-	energy		: COM energy of beam of particles
-							allegedly being simulated
-	helicity	: helicity of the alleged beam
-output :
-						: cross-section of scattering
+In the toy example given in the tutorial, we imagine that this function 
+represents black box simulation software for nuclear cross-section
+calculations. The students are instructed to find the maximum of this function
+using a Nelder-Mead optimizer method (written for the tutorial, and 
+included in this directory) to find the 'parameter values' 
+('energy' and 'helicity') that give the largest cross-section.
 
-In the toy example given in the tutorial,
-we imagine that this function actually represents
-simulation software for nuclear cross-section
-calculations. A driver program ('find_best_params.py')
-uses this function along with a Nelder-Mead
-optimizer method (included in this directory)
-to find the 'parameter values' 
-('energy' and 'helicity')
-that give the largest cross-section.
+In reality, this is simply the negative of Himmelblau's function.
 """
 
-from math import sqrt
+def simulate(params):
+  """
+  Argument:
+    params - numpy.array([energy, helicity]), where
+      energy = COM energy of beam of particles allegedly being simulated
+      helicity = helicity of the alleged beam
+  Return value:
+    cross-section of scattering
+  """
 
-def simulate(energy, helicity):
-	return sqrt(\
-				(energy**3 - 3*energy*(helicity**2) - 1)**2 + \
-				(3*helicity*(energy**2) - helicity**3)**2 \
-			) - \
-			energy + 10
+  himmel = (params[0]**2 + params[1] - 11)**2 + (params[0] + params[1]**2 - 7)**2
+  return -himmel
+
+if __name__ == '__main__':
+
+  import numpy as np
+
+  # Test one known minimum
+  test_out_1 = simulate(np.array([-2.805118,3.131312]))
+  assert test_out_1 > -1e-10
+
+  # Test the known maximum
+  test_out_2 = simulate(np.array([-0.270845, -0.923039]))
+  assert test_out_2 < -180
+
+  # Test a very-precisely-known minimum
+  # (even accounting for floating-point
+  # comparison, this should test fine)
+  test_out_3 = simulate(np.array([3.0, 2.0]))
+  assert test_out_3 == 0.0
+
